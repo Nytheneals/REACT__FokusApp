1e30bee7d955089c852e82f2f1fe548bb5828157
Nytheneals <nythes@gmail.com>
2018-02-26 23:02:38 -0500

    Hooked up react Router

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,21 +1,32 @@
-import React, { Component } from "react";
-// import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
+// COMPONENTS
 // import PhotoGrid from "./PhotoGrid";
 // import SingleView from "./SingleView";
+// CSS
+import '../css/_animations.css';
+import '../css/_normalise.css';
+import '../css/_style.css';
+import '../css/_topography.css';
 
-import "../css/_animations.css";
-import "../css/_normalise.css";
-import "../css/_style.css";
-import "../css/_topography.css";
-
+// COMPONENT
 class Main extends Component {
   render() {
     return (
-      <div className="photo-grid">
-        <h1>kirstegram</h1>
-      </div>
+      <Router>
+        <Route>
+          <h1>
+            <Link to="/">Kirste-gram</Link>
+            {React.cloneElement(this.props.children, this.props)}
+          </h1>
+        </Route>
+      </Router>
     );
   }
 }
 
 export default Main;
+
+// {React.cloneElement(this.props.children, this.props)}
+// THIS CLONES THE COMPONENTS THAT ARE THE CHILDREN OF THE MAIN COMPONENT INCLUDING THERE PARTICULAR PROPS.
--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import Main from "./components/Main";
-import registerServiceWorker from "./registerServiceWorker";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import './css/_animations.css';
+import './css/_normalise.css';
+import './css/_style.css';
+import './css/_topography.css';
+import Main from './components/Main';
+// import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<Main />, document.getElementById("root"));
-registerServiceWorker();
+ReactDOM.render(<Main />, document.getElementById('root'));
+// registerServiceWorker();
