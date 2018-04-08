3d659b9753665628a1a59d86df7f73f55283fc2f
Nytheneals <nythes@gmail.com>
2018-04-07 23:28:50 -0400

    Added Webpack

--- src/index.js
+++ src/index.js
@@ -1,31 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
 // CSS
-import './index.css';
-import './css/_animations.css';
-import './css/_normalise.css';
-import './css/_style.css';
-import './css/_topography.css';
+import "./index.css";
+import "./css/_animations.css";
+import "./css/_normalise.css";
+import "./css/_style.css";
+import "./css/_topography.css";
 
 // COMPONENTS
 
-import Main from './components/Main';
+import Main from "./components/Main";
 import PhotoGrid from "./components/PhotoGrid";
 import SingleView from "./components/SingleView";
 
 // DEPENDENCIES(REACT-ROUTER)
-import {Router, route} from 'react-router-dom'
+import { Router, route } from "react-router-dom";
 // import {Router, Route, IndexRoute, browserHistory} from 'react-router-dom';
 const router = (
- <Router>
-  <Route path="/" component ={Main}>
-   <IndexRoute component={PhotoGrid}></IndexRoute>
-   <Route path='/view/:postId' component={SingleView}></Route>
-  </Route>
- </Router>
-)
+  <Router>
+    <Route path="/" component={Main} />
+    <IndexRoute component={PhotoGrid} />
+    <Route path="/view/:postId" component={SingleView} />
+  </Router>
+);
 
-ReactDOM.render(
- <router/>, document.getElementById('root'));
+ReactDOM.render(<router />, document.getElementById("root"));
 // registerServiceWorker();
